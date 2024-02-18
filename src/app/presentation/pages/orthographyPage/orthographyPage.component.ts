1b94d84c0d99f63ece03c1c79710e0155ad27117
IrvingHdz92 <irvinghdz_isc92@outlook.com>
2024-02-17 18:30:45 -0600

    First version of code

---
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TextMessageBoxEvent, TextMessageBoxFileComponent, TextMessageBoxSelectComponent, TextMessageEvent, TypingLoaderComponent } from '@components/index';
+import { Message } from 'app/interfaces';
+import { OpenAiService } from 'app/presentation/services/openai.service';
+
+@Component({
+  selector: 'app-orthography-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
+    TextMessageBoxSelectComponent
+  ],
+  templateUrl: './orthographyPage.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class OrthographyPageComponent {
+
+  public messages = signal<Message[]>([{ text: 'Hola mundo', isGpt: false }]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiService);
+
+  handleMessage( prompt: string) {
+
+    console.log({ prompt });
+
+  }
+
+  handleMessageWithFile( { prompt, file }: TextMessageEvent ) {
+
+    console.log({ prompt, file });
+
+  }
+
+  handleMessageWithSelect( event: TextMessageBoxEvent ) {
+
+  }
+
+}
